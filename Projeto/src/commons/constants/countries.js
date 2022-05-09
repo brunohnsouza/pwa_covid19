356d20d96712502c3cabd3deea11e9023e5896bd
Urb0n <brunohenriquehns@gmail.com>
2022-05-09 20:02:42 -0300

    Projetos e seus arquivos e pacotes

---
+++ Projeto/src/commons/constants/countries.js
@@ -0,0 +1,65 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import ArgentinaFlag from '../../assets/images/argentina.png'
+import CanadaFlag from '../../assets/images/canada.png'
+import FranceFlag from '../../assets/images/france.png'
+import JapanFlag from '../../assets/images/japan.png'
+import UkFlag from '../../assets/images/uk.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'Argentina',
+    value: 'argentina',
+    flag: ArgentinaFlag
+  },
+  {
+    label: 'Canadá',
+    value: 'canada',
+    flag: CanadaFlag
+  },
+  {
+    label: 'França',
+    value: 'france',
+    flag: FranceFlag
+  },
+  {
+    label: 'Japão',
+    value: 'japan',
+    flag: JapanFlag
+  },
+  {
+    label: 'Reino Unido',
+    value: 'uk',
+    flag: UkFlag
+  }
+]
+
+export default COUNTRIES
