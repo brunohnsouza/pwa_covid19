95a761b7aaf998595ebea50e18f38249be7c9927
Bruno Henrique <103577843+brunohnsouza@users.noreply.github.com>
2022-05-10 16:42:21 -0300

    Update Panel.jsx

--- Projeto/src/containers/Main/components/Panel.jsx
+++ Projeto/src/containers/Main/components/Panel.jsx
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 const navigatorHasShare = navigator.share;
 
 function Panel({ updateAt, onChange, data, country, getCoviddata}) {
-    const { cases, recovered, deaths, todayCases, todayDeaths } = data;
+    const { recovered } = data;
 
     const renderCountries = (country, index) => (
         <MenuItem key={`country-${index}`} value={country.value}>
